f9d90774a8e372f1fc766f094a3a02c17937cc97
Jon DeJong <jon.dejong@gmail.com>
2017-01-09 10:04:38 -0600

    removing unneeded bootstrap components

--- ng-client/src/app/employee/employee.module.ts
+++ ng-client/src/app/employee/employee.module.ts
@@ -17,7 +17,6 @@ import {EmployeeService} from './employee.service'
   ],
   exports: [
   ],
-  providers: [EmployeeService],
-  bootstrap: []
+  providers: [EmployeeService]
 })
 export class EmployeeModule { }
